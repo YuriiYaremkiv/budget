60c3a914e1caabc7456474221d0f841798689818
yyaremkiv <y.yaremkiv@gmail.com>
2023-04-21 15:01:08 +0300

    use deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from 'assets/location/react-i18next';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename="/budget">
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
+      </PersistGate>
+    </Provider>
+  </BrowserRouter>
 );
